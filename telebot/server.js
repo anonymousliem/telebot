8a47ed58272338bb53ddb6409425be41bb437922
anonymousliem <muslimsyuhadah@gmail.com>
2021-04-01 01:28:19 +0700

    add directory bot

---
+++ telebot/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
+var cors = require("cors");
+var uuid = require("uuid");
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+//create database connection
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+//connect to database
+conn.connect((err) => {
+  if (!err) {
+    console.log("Connected");
+  } else {
+    console.log("Connection Failed");
+  }
+});
+
+//Server listening
+var port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("Server started on port 4000...");
+});
